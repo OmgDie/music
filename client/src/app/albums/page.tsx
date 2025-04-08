1fbf330e8018eb0608434b5743550d6815d523c1
OmgD1e <Simplex.ru@mail.ru>
2025-04-08 05:07:37 +0300

    feat: add page routing

---
+++ client/src/app/albums/page.tsx
@@ -0,0 +1,8 @@
+export default function Albums() {
+    return (
+      <div>
+        <h1>Albums</h1>
+        <p>Explore the latest albums here!</p>
+      </div>
+    );
+  }
---
+++ client/src/app/tracks/page.tsx
@@ -0,0 +1,8 @@
+export default function Tracks() {
+    return (
+      <div>
+        <h1>Tracks</h1>
+        <p>Explore the latest tracks here!</p>
+      </div>
+    );
+  }
